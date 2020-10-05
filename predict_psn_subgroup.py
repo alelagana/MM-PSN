eb02af1ca8c9408d4d221dbd5ca52f0fe28d35e7
sherry308 <s.sherry308@gmail.com>
2020-10-05 14:51:32 -0400

    updated MM-PSN Files

--- predict_psn_subgroup.py
+++ predict_psn_subgroup.py
@@ -1,21 +1,16 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 import os
 import re
 import sys
 import pandas as pd
-pd.options.mode.chained_assignment = None 
 import numpy as np
+pd.options.mode.chained_assignment = None
 from sklearn.svm import LinearSVC
 from sklearn.svm import SVC
 from itertools import cycle
 from xgboost import XGBClassifier
 from sklearn.preprocessing import StandardScaler
 from dummyPy import OneHotEncoder
+#from dummyPy import OneHotEncoder
 from sklearn.ensemble import RandomForestClassifier
 import joblib
 
@@ -25,42 +20,39 @@ import joblib
 exp_file = sys.argv[1]
 cnv_file = sys.argv[2]
 trans_file = sys.argv[3]
+out_file = sys.argv[4]
+
 trans=pd.read_csv(trans_file,index_col=0)
 cnv=pd.read_csv(cnv_file,index_col=0)
 exp=pd.read_csv(exp_file,index_col=0)
 
 ### these files are the features required by this code for predicting the class
 
-with open('expression_features.tsv') as f:
-    exp_list = f.read().splitlines()
+with open('expression_features_rem.tsv') as f:
+        exp_list = f.read().splitlines()
 with open('CNV_features.tsv') as f:
-    cnv_list = f.read().splitlines()
+        cnv_list = f.read().splitlines()
 with open('translocation_features.tsv') as f:
-    trans_list= f.read().splitlines()
-    
+        trans_list= f.read().splitlines()
+
 cc=cnv.columns.values.tolist()
 c=len(list(set(cc).intersection(set(cnv_list))))
+
 if c != 50:
-        print("CNV Feature are missing. Please make sure all the features in  CNV_features.tsv is present in your Copy number variation input file ") # you will get an error
-            
-cc=exp.columns.values.tolist()
-c=len(list(set(cc).intersection(set(exp_list))))
-if c != 109:
-    print("Expression Feature are missing. Please make sure all the features in  expression_features.tsv is present in your expression input file ") # you will get an error
-                        
+            print("CNV Feature are missing. Please make sure all the features in  CNV_features.tsv is present in your Copy number variation input file ") # you will get an error
 cc=trans.columns.values.tolist()
 c=len(list(set(cc).intersection(set(trans_list))))
 if c != 8:
-    print("Translocations Feature are missing. Please make sure all the features in  translocation_features.tsv is present in your translocation input file ") # you will get an error
-# selecting the required features from  all the features 
+        print("Translocations Feature are missing. Please make sure all the features in  translocation_features.tsv is present in your translocation input file ") # you will get an error
+        # selecting the required features from  all the features 
+
 sel_exp=exp[exp_list]
 sel_cnv=cnv[cnv_list]
 sel_trans=trans[trans_list]
 
-
 ### load the scaler and z score the vst normalised counts
 
-filename = 'scaler.sav'
+filename = 'scaler1.sav'
 sc = joblib.load(filename)
 sel_exp_scaled=sc.transform(sel_exp)
 sel_exp_scaled = pd.DataFrame(sel_exp_scaled, index=sel_exp.index, columns=sel_exp.columns)
@@ -72,37 +64,51 @@ names = ['0','1','2']
 sel_exp_bin=sel_exp_scaled.apply(lambda x: pd.cut(x, bins,labels=names), axis=0)
 
 ### OneHot encode the expression data
-filename = 'encoder.sav'
+filename = 'encoder1.sav'
 encoder1 = joblib.load(filename)
 exp_encoded=encoder1.transform(sel_exp_bin)
-exp_encoded.index=sel_exp_bin.index
+exp_encoded1 = pd.DataFrame(exp_encoded.toarray())
+exp_encoded1.index=sel_exp_bin.index
+
+type(exp_encoded1)
+
+exp_encoded1.columns=encoder1.get_feature_names(sel_exp_bin.columns)
+exp_encoded1.iloc[0:5,0:5]
 
 ### Convert CNV data to bins
 bins = [0, 0.5,1.5, 2.5, 3.5,np.inf]
+#bins = [-np.inf,1.5, 2.5,np.inf]
 names = ['0','1','2','3','4']
+#names = ['0','1','2']
 sel_cnv_bin=sel_cnv.apply(lambda x: pd.cut(x, bins,labels=names), axis=0)
-
+#sel_cnv.to_csv("sel_cnv_w")
+#sel_cnv_bin.to_csv("sel_cnv_bin_w")
 ## onehot encoding of CNV data
-filename = 'encoder_cnv.sav'
+filename = 'encoder_cnv1.sav'
 encoder_cnv = joblib.load(filename)
-#encoder_cnv = load(open('onehotencoder_cnv.pkl', 'rb'))
 cnv_encoded=encoder_cnv.transform(sel_cnv_bin)
+
+cnv_encoded1 = pd.DataFrame(cnv_encoded.toarray())
+cnv_encoded1.index=sel_cnv_bin.index
+
+type(cnv_encoded1)
+
+cnv_encoded1.columns=encoder_cnv.get_feature_names(sel_cnv_bin.columns)
+#cnv_encoded1.iloc[0:5,0:5]
 cnv_encoded.index=sel_cnv_bin.index
 
-
 ### Join the different omics 
-test = pd.concat([cnv_encoded,exp_encoded, trans, ], axis=1, sort=False)
-test = test.infer_objects() 
-
+test = pd.concat([cnv_encoded1,exp_encoded1, sel_trans], axis=1, sort=False)
+test = test.infer_objects()
+test.to_csv("test")
 
 ### load the model and predict the sample and save the file
 
-filename = 'Model.sav'
+filename = 'Model1.sav'
 clf = joblib.load(filename)
 predic_test=clf.predict(test)
 predict_test_data = pd.DataFrame(predic_test, index=test.index)
 predict_test_data.columns = ['subGroup']
-
 predict_test_data['Subgroup'] = '1a'
 predict_test_data['Subgroup'][predict_test_data['subGroup'] == 2] = '1b'
 predict_test_data['Subgroup'][predict_test_data['subGroup'] == 3] = '1c'
@@ -120,5 +126,8 @@ predict_test_data['Subgroup'][predict_test_data['subGroup'] == 1] = '1a'
 df=predict_test_data['Subgroup']
 df = pd.DataFrame(df)
 
-df.to_csv("Predicted_class.csv")
+#df.to_csv("Predicted_class.csv")
+df.to_csv(out_file)
+
+
 
